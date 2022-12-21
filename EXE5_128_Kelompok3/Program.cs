d3cd836b05c7121643095851615445cd325d27a2
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-12-21 16:43:16 +0700

    menambahkan switch pada static void main

--- EXE5_128_Kelompok3/Program.cs
+++ EXE5_128_Kelompok3/Program.cs
@@ -58,7 +58,46 @@ namespace Exe3_128_Kelompok3
             char ch;
             while (true)
             {
-
+                try
+                {
+                    Console.WriteLine("Menu");
+                    Console.WriteLine("1. Memasukkan Data");
+                    Console.WriteLine("2. Menghapus Data");
+                    Console.WriteLine("3. Menampilkan Data");
+                    Console.WriteLine("4. Keluar");
+                    Console.WriteLine("\nMasukkan Pilihan Anda (1-4): ");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                q.insert();
+                                break;
+                            }
+                        case '2':
+                            {
+                                q.delete();
+                                break;
+                            }
+                        case '3':
+                            {
+                                q.display();
+                                break;
+                            }
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Opsi Tidak Valid!!");
+                                break;
+                            }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Periksa nilai yang dimasukkan");
+                }
             }
         }
     }
